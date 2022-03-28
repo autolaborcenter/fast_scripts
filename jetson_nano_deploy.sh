53b362e37d573d673d553862616288eadc553c7f
ThinkPad-T460P <zhangchengkai@autolabor.com.cn>
2022-03-28 14:15:40 +0800

    update

--- jetson_nano_deploy.sh
+++ jetson_nano_deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-echo "Deploy environment quickly for jetson nano (Jetpack 4.6.1)"
-
-
-echo "1.Initial workspace"
+echo "Initial workspace: ${HOME}/fs_ws/"
 # workspace : ${HOME}/fs_ws/
 cd ${HOME}
 if [ ! -d "fs_ws" ]; then
@@ -12,56 +9,86 @@ if [ ! -d "fs_ws" ]; then
 fi
 
 
-echo "2.Change mirror source(Ubuntu 18.04) to USTC.edu.cn"
+function change_source {
+echo "Change mirror source(Ubuntu 18.04) to USTC.edu.cn"
 
+wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/sources.list && \
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
+mv sources.list /etc/apt/sources.list;
+apt update;
+}
 
-cat >/etc/apt/sources.list<<EOF
-# 默认注释了源码仓库，如有需要可自行取消注释
-deb https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic main restricted universe multiverse
-# deb-src https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic main main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-updates main restricted universe multiverse
-# deb-src https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-updates main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-backports main restricted universe multiverse
-# deb-src https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-backports main restricted universe multiverse
-deb https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-security main restricted universe multiverse
-# deb-src https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-security main restricted universe multiverse
+function enable_mcp251x {
+echo "Enable dtbo file for MCP251x CAN controller"
 
-# 预发布软件源，不建议启用
-# deb https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-proposed main restricted universe multiverse
-# deb-src https://mirrors.ustc.edu.cn/ubuntu-ports/ bionic-proposed main restricted universe multiverse
-EOF
+wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/mcp251x/jetson-mcp251x.dtbo && \
+cp jetson-mcp251x.dtbo /boot;
+mv tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo.bak
+mv tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo.bak
+/opt/nvidia/jetson-io/config-by-hardware.py -n "MCP251x CAN Controller"
 
-echo "3.Update your apt software list"
-apt update
+}
 
-echo "4.Install some software"
-apt install nano nload htop
+function test_mcp251x{
+echo "Test MCP251x CAN controller "
 
-wget - https://download.nomachine.com/download/7.8/Arm/nomachine_7.8.2_1_arm64.deb && dpkg -i nomachine*.deb
+sh -c "$(wget https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/cantest.sh -O -)"
+}
 
+function install_zsh {
+echo "Install zsh & omz & config "
 
-echo "5.Install ZSH"
 apt install zsh
 
-echo "6.Install oh-my-zsh and plugin"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-source .zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting 
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo "7.Update .zshrc file"
-wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/.zshrc
 mv ~/.zshrc ~/.zshrc_bkp
-cp .zshrc ~/.zshrc
-
+wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/.zshrc &&\
+cp .zshrc ~/.zshrc;
 source .zshrc
+}
 
-echo "8.Update dtb for CAN"
-cp jetson-mcp251x.dtbo /boot
-/opt/nvidia/jetson-io/config-by-hardware.py -n "MCP251x CAN Controller"
-echo "Restart after 10 seconds."
-sleep 10s
-reboot
+function install_utils {
+echo "Install utils"
+
+apt install nano nload htop
+}
+function install_nomachine {
+echo "Install Nomachine"
+
+wget - https://download.nomachine.com/download/7.8/Arm/nomachine_7.8.2_1_arm64.deb && \
+dpkg -i nomachine*.deb;
+}
+
+PS3="Enter an option: "
+select option in  "Change source"\
+                  "Enable MCP251x"\
+                  "Test MCP251x"\
+                  "Install zsh"
+                  "Install utils" \
+                  "Install Nomachine" \
+do
+	case $option in
+	"Change source")
+		change_source ;;
+	"Enable MCP251x")
+		enable_mcp251x ;;
+	"Test MCP251x")
+		test_mcp251x ;;
+	"Install zsh"）
+		install_zsh ;;
+	"Install utils"）
+		install_utils ;;
+	"Install Nomachine"）
+		install_nomachine ;;
+	*)
+		clear
+		echo "Sorry, wrong selection";;
+	esac
+done
+clear
+
+ 
