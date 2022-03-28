778880c4503a3317e2ce8065d1c3e56f28ee1a66
book诗意 <tech.gosling0d@icloud.com>
2022-03-28 17:48:00 +0800

    Update jetson_nano.sh

--- jetson_nano.sh
+++ jetson_nano.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
- change_source (){
+change_source (){
     echo "Change mirror source(Ubuntu 18.04) to USTC.edu.cn"
     wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/sources.list && \
     mv /etc/apt/sources.list /etc/apt/sources.list.bak
@@ -9,31 +9,37 @@
     apt update;
 }
 
- update_pl2303 (){
+update_pl2303 (){
     echo "Update UNITEK's PL2303 driver"
     wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/pl2303/pl2303.ko && \
     rmmod /lib/modules/4.9.253-tegra/kernel/drivers/usb/serial
-    cp pl2303.ko /lib/modules/4.9.253-tegra/kernel/drivers/usb/serial/
+    mv pl2303.ko /lib/modules/4.9.253-tegra/kernel/drivers/usb/serial/
     insmod /lib/modules/4.9.253-tegra/kernel/drivers/usb/serial
 }
 
- enable_mcp251x (){
+initial_mcp251x (){
     echo "Enable dtbo file for MCP251x CAN controller"
     wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/mcp251x/jetson-mcp251x.dtbo && \
-    cp jetson-mcp251x.dtbo /boot;
+    mv jetson-mcp251x.dtbo /boot;
     mv tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo.bak
     mv tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo.bak
     /opt/nvidia/jetson-io/config-by-hardware.py -n "MCP251x CAN Controller"
 
 }
+initial_camera (){
+    echo "Enable dtbo file for mipi CSI camera"
+#     wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/mcp251x/jetson-mcp251x.dtbo && \
+#     mv jetson-mcp251x.dtbo /boot;
+#     mv tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a02-mcp251x.dtbo.bak
+#     mv tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo /boot/tegra210-p3448-0000-p3449-0000-a01-mcp251x.dtbo.bak
+#     /opt/nvidia/jetson-io/config-by-hardware.py -n "MCP251x CAN Controller"
 
- test_socket_can (){
+}
+test_socket_can (){
     echo "Test socket CAN "
     echo "set CAN bitrate 250000"
     sudo ifconfig can0 down
     sudo ip link set can0 type can bitrate  250000
-
-
 
     i=0; 
     while [ $i -le 5 ]; 
@@ -52,7 +58,7 @@
     done
 }
 
- install_zsh (){
+install_zsh (){
     echo "Install zsh & omz & config "
 
     apt install zsh
@@ -64,17 +70,17 @@
 
     mv ~/.zshrc ~/.zshrc_bkp
     wget - https://raw.githubusercontent.com/autolaborcenter/fast_scripts/main/.zshrc &&\
-    cp .zshrc ~/.zshrc;
+    mv .zshrc ~/.zshrc;
     source .zshrc
 }
 
- install_utils (){
+install_utils (){
     echo "Install utils"
 
     apt install nano nload htop
 }
 
- install_nomachine (){
+install_nomachine (){
     echo "Install Nomachine"
 
     wget - https://download.nomachine.com/download/7.8/Arm/nomachine_7.8.2_1_arm64.deb && \
@@ -84,12 +90,11 @@
 ##################################################
 
 echo "Initial workspace: ${HOME}/fs_ws/"
-# workspace : ${HOME}/fs_ws/
 cd ${HOME}
 if [ ! -d "fs_ws" ]; then
   mkdir fs_ws
-  cd fs_ws
 fi
+cd fs_ws
 
 TERMINAL=/dev/tty
 HEIGHT=15
@@ -101,11 +106,12 @@ MENU="Choose one operation of the following options:"
 
 OPTIONS=(1 "Change source" 
          2 "Update PL2303" 
-         3 "Enable MCP251x" 
-         4 "Test socket CAN" 
-         5 "Install zsh" 
-         6 "Install utils" 
-         7 "Install Nomachine")
+         3 "Initial MCP251x" 
+         4 "Initial camera" 
+         5 "Test socket CAN" 
+         6 "Install zsh" 
+         7 "Install utils" 
+         8 "Install Nomachine")
 
 CHOICE=$(whiptail --clear \
                 --backtitle "$BACKTITLE" \
@@ -124,18 +130,21 @@ case $CHOICE in
             update_pl2303
             ;;
         3)
-            enable_mcp251x
+            initial_mcp251x
             ;;
         4)
-            test_socket_can
+            initial_camera
             ;;
         5)
-            install_zsh
+            test_socket_can
             ;;
         6)
-            install_utils
+            install_zsh
             ;;
         7)
+            install_utils
+            ;;
+        8)
             install_nomachine
             ;;
         *)
